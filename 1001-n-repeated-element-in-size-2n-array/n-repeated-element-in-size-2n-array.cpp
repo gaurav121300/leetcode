48c808f6af6e50d02cb1d0bfda7a1b7b04fc5531
gaurav121300 <143287813+gaurav121300@users.noreply.github.com>
2026-01-03 01:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (97.62%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        for(auto num : nums){
-            mpp[num]++;
-        }
-        for(auto it = mpp.begin();it != mpp.end();it++){
-            if(it->second > 1) return it->first;
+        if(nums[0] == nums[nums.size()-1]) return nums[0];
+        for(int i = 0; i<nums.size();i++){
+            if(i+1>=nums.size()) continue;
+          if(nums[i] == nums[i+1]) return nums[i];
+          if(i+2<nums.size() && nums[i] == nums[i+2]) return nums[i];
         }
         return -1;
     }
