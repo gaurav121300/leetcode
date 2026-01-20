9726dd71dde3b2764d7b6a2123725876b578774d
gaurav121300 <143287813+gaurav121300@users.noreply.github.com>
2026-01-20 22:01:01 +0530

    Time: 907 ms (5.36%) | Memory: 29.6 MB (92.41%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int getRect(const vector<vector<int>>& P, int x1, int y1, int x2, int y2) {
-        return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1];
+    int findSum(int startRow,int startCol,int endRow,int endCol,vector<vector<int>>& mat){
+        int sum = 0;
+
+        for(int i = startRow;i <= endRow;i++){
+            for(int j = startCol; j <= endCol; j++){
+                sum += mat[i][j];
+            }
+        }
+        return sum;
     }
-
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> P(m + 1, vector<int>(n + 1));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] +
-                          mat[i - 1][j - 1];
-            }
-        }
+        int m = mat.size();
+        int n = mat[0].size();
 
-        int l = 1, r = min(m, n), ans = 0;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            bool find = false;
-            for (int i = 1; i <= m - mid + 1; ++i) {
-                for (int j = 1; j <= n - mid + 1; ++j) {
-                    if (getRect(P, i, j, i + mid - 1, j + mid - 1) <=
-                        threshold) {
-                        find = true;
-                    }
+        int maxlen = 0;
+
+        for(int i = 0; i < m;i++){
+            int bestSide = 0;
+            for(int j = 0; j < n;j++){
+                for(int offset = 0;offset < min(m-i,n-j);offset++){
+                    int r2 = i + offset;
+                    int c2 = j + offset;
+
+                    int val = findSum(i,j,r2,c2,mat);
+                    if(val <= threshold){
+                        bestSide = offset + 1;
+                    }else break;
                 }
+                maxlen = max(maxlen,bestSide);
             }
-            if (find) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
+            
         }
-        return ans;
+        return maxlen;
     }
 };
