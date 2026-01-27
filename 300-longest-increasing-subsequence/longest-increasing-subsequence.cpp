bbd9969f67b025dfde3b0d32e3be265b37441bc9
gaurav121300 <143287813+gaurav121300@users.noreply.github.com>
2026-01-27 05:58:09 +0530

    Time: 283 ms (31.58%) | Memory: 296.4 MB (20.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-       int helper(int ind,int prev_ind,vector<int>&nums,vector<vector<int>> &dp){
-           if(ind == nums.size()) return 0;
-
-           if(dp[ind][prev_ind + 1] != -1) return dp[ind][prev_ind+1];
-        int len = 0,take = 0;
-        int notTake = 0 + helper(ind+1,prev_ind,nums,dp);
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-         take = 1 + helper(ind + 1,ind,nums,dp);
-        }
-        len = max(notTake,take);
-        return dp[ind][prev_ind + 1] = len;
-    // }
-       }
-    // int helper(int ind,int prev_ind,vector<int> &nums){
-    //     if(ind == nums.size()) return 0;
-    //     int len = 0,take = 0;
-    //     int notTake = 0 + helper(ind+1,prev_ind,nums);
-    //     if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-    //      take = 1 + helper(ind + 1,ind,nums);
-    //     }
-    //     len = max(notTake,take);
-    //     return len;
-    // }
     int lengthOfLIS(vector<int>& nums) {
-        // int ans = helper(0,-1,nums);
-        // return ans;
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return helper(0,-1,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int ind = n-1;ind>=0;ind--){
+            for(int prev_ind = ind-1;prev_ind >= -1;prev_ind--){
+                int len = 0 + dp[ind+1][prev_ind+1];
+                if(prev_ind == -1 || nums[ind] > nums[prev_ind])
+                len = max(len,1 + dp[ind+1][ind+1]);
+            
+            dp[ind][prev_ind+1] = len;
+            }
+        }
+        return dp[0][0];
     }
 };
