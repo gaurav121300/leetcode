739104ac72517990041df8731ff8ba318b523ac2
gaurav121300 <143287813+gaurav121300@users.noreply.github.com>
2026-09-01 19:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (15.32%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int hLen = haystack.length();
-        int nLen = needle.length();
-        int nIndex = 0;
-        for(int i=0; i<hLen; i++){
-            // as long as the characters are equal, increment needleIndex
-            if(haystack[i]==needle[nIndex]){
-                nIndex++;
-            }
-            else{
-                // start from the next index of previous start index
-                i=i-nIndex;
-                // needle should start from index 0
-                nIndex=0;
-            }
-            // check if needleIndex reached needle length
-            if(nIndex==nLen){
-                // return the first index
-                return i-nLen+1;
-            }
+        int n = haystack.size();
+        int m = needle.size();
+
+        for(int i = 0; i <=  n - m;i++){
+            if(haystack.substr(i,m) == needle)
+            return i;
         }
         return -1;
     }
